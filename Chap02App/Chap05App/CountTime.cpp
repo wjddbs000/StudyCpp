12cbded4fd53319baf1df0483d357a956048e5b5
song <wjddbs000@naver.com>
2020-04-24 11:16:00 +0900

    5장 연산자 오버로드

---
+++ Chap02App/Chap05App/CountTime.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Time {
+	friend ostream& operator <<(ostream& c, const Time& T);
+	friend ostream& operator <<(ostream& c, const Time* pT);
+private:
+	int hour, min, sec;
+public:
+	Time() { ; }
+	Time(int h, int m, int s) { hour = h; min = m; sec = s; }
+	void OutTime() {
+		printf("%d:%d:%d\n", hour, min, sec);
+	}
+
+};
+
+ostream& operator <<(ostream& c, const Time& T)
+{
+	c << T.hour << "시" << T.min << "분" << T.sec << "초";
+	return c;
+}
+
+ostream& operator <<(ostream& c, const Time* pT)
+{
+	c << *pT;
+	return c;
+}
+
+int main() {
+	Time now(11, 22, 33);
+	Time* pLunchTime = new Time(12, 30, 0);
+	cout << "현재 시간은" << now <<"입니다"<< endl;
+	cout << "현재 시간은" << pLunchTime << "입니다" << endl;
+}
---
+++ Chap02App/Chap05App/TimePlusPlus.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+class Time {
+private:
+	int hour, min, sec;
+public:
+	Time() { ; }
+	Time(int h, int m, int s) : hour(h), min(m), sec(s) { ; }
+	void OutTime() {
+		printf("%d:%d:%d\n", hour, min, sec);
+	}
+	const Time operator ++() {
+		*this;
+		sec++;
+		min += sec / 60;
+		sec %= 60;
+		hour += min / 60;
+		min %= 60;
+		return *this;
+	}
+	const Time operator ++(int dummy) {
+		Time t = *this;
+		++* this;
+		return t;
+	}
+};
+
+int main() {
+	Time t1(1, 1, 1);
+	Time t2;
+
+	t2 = ++t1;
+	t1.OutTime();
+	t2.OutTime();
+
+	t2 = t1++;
+	t1.OutTime();
+	t2.OutTime();
+
+}
